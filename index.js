88c62376d75bd9054977da9f07108185025f5747
Thomas Broadley <buriedunderbooks@hotmail.com>
2016-03-24 16:32:09 -0400

    my first component!

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import App from './components/app';
+
 ReactDOM.render(
-  <div>Hello world</div>,
+  <App/>,
   document.getElementById('root')
 );
