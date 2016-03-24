6a9d78fa0ef7b31d5828d8702bb8a79286e85d62
Thomas Broadley <buriedunderbooks@hotmail.com>
2016-03-24 16:50:56 -0400

    added some more components for the calendar

--- components/calendar.js
+++ components/calendar.js
@@ -3,9 +3,42 @@ import React from 'react';
 export default class Calendar extends React.Component {
   render() {
     return (
-      <div>
-        Date: {this.props.events[0].date}
+      <div id="calendar">
+        <CalendarHeader />
+        {[0, 1, 2, 3, 4].map(
+          _ => <CalendarRow />
+        )}
       </div>
     );
+  }
+}
+
+class CalendarHeader extends React.Component {
+  render() {
+    return (
+      <div>
+        {["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"].map(
+          day => (<div><h2>{day}</h2></div>)
+        )}
+      </div>
+    );
+  }
+}
+
+class CalendarRow extends React.Component {
+  render() {
+    return (
+      <div>
+        {[0, 1, 2, 3, 4, 5, 6].map(
+          _ => <CalendarCell />
+        )}
+      </div>
+    );
+  }
+}
+
+class CalendarCell extends React.Component {
+  render() {
+    return <div></div>;
   }
 }
