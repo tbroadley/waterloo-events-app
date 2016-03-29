86ee91433678498f7bf0bdd94f90e75651bc3f90
Thomas Broadley <buriedunderbooks@hotmail.com>
2016-03-29 16:36:14 -0400

    added a button to close the event detail pane

--- components/event-detail.js
+++ components/event-detail.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import { changeSelectedEvent } from '../actions/main';
+
 class EventDetail extends React.Component {
   render() {
     if (this.props.selectedEvent === undefined) {
@@ -14,8 +16,15 @@ class EventDetail extends React.Component {
         date,
       } = this.props.selectedEvent;
 
+      const onDeselect = _ => this.props.dispatch(changeSelectedEvent(-1));
+
       return (
         <div id='event-detail'>
+          <button
+            onClick={onDeselect}
+          >
+            Close
+          </button>
           <div>{name}</div>
           <div>{date.toString()}</div>
         </div>
