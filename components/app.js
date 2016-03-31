8474d7302f19b4d347786a223012ff88b16984af
Thomas Broadley <buriedunderbooks@hotmail.com>
2016-03-31 18:04:06 -0400

    added a link to a contact page

--- components/app.js
+++ components/app.js
@@ -6,9 +6,15 @@ import EventDetail from './event-detail';
 export default class App extends React.Component {
   render() {
     return (
-      <div id='app'>
-        <Calendar />
-        <EventDetail />
+      <div id='app-parent'>
+        <div id='header'>
+          <a href='/'>Calendar</a>
+          <a href='/contact.html'>Contact</a>
+        </div>
+        <div id='app'>
+          <Calendar />
+          <EventDetail />
+        </div>
       </div>
     );
   }
